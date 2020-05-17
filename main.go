b601164ad137af982c5ad31f58482dfa9bea187f
Daniel Lwetabe <acehanks@gmail.com>
2020-05-17 09:59:55 +0300

    create and return a JWT token after authentication

--- main.go
+++ main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
 type User struct {
-	ID uint64 `json:"id"`
+	ID       uint64 `json:"id"`
 	Username string `json:username`
 	Password string `json:password`
 }
 
-var user = User {
-	ID: 1,
+var user = User{
+	ID:       1,
 	Username: "admin",
 	Password: "admin",
 }
@@ -32,7 +35,8 @@ func main() {
 	log.Fatal(router.Run(":8080"))
 }
 
-func Login(c *gin.Context){
+// Login authenticates a user a returns a JWT token
+func Login(c *gin.Context) {
 	var userLogin User
 	if err := c.ShouldBindJSON(&userLogin); err != nil {
 		c.JSON(http.StatusUnprocessableEntity, "Invalid Json provided")
@@ -43,4 +47,31 @@ func Login(c *gin.Context){
 		c.JSON(http.StatusUnauthorized, "Please provide valid login credentials")
 		return
 	}
+
+	token, err := CreateToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
+}
+
+// CreateToken function generates and returns a jwt token
+func CreateToken(userid uint64) (string, error) {
+	var err error
+	os.Setenv("ACCESS_SECRET", "fwihbfikwfbhi") // TODO: should be set in .env
+
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = userid
+	claims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+
+	sign := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	token, err := sign.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
