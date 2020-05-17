b1c05c4c39af7a40ff04e259781a6b8b2bd84bbe
Daniel Lwetabe <acehanks@gmail.com>
2020-05-17 09:35:43 +0300

    add authentication credentials check

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	ID uint64 `json:"id"`
+	Username string `json:username`
+	Password string `json:password`
+}
+
+var user = User {
+	ID: 1,
+	Username: "admin",
+	Password: "admin",
+}
 
 func main() {
 	router := gin.Default()
@@ -9,6 +26,21 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	router.POST("/login", Login)
 	// runs on 0.0.0.0:8080 for windows
-	router.Run()
+	log.Fatal(router.Run(":8080"))
+}
+
+func Login(c *gin.Context){
+	var userLogin User
+	if err := c.ShouldBindJSON(&userLogin); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid Json provided")
+		return
+	}
+
+	if userLogin.Username != user.Username || userLogin.Password != user.Username {
+		c.JSON(http.StatusUnauthorized, "Please provide valid login credentials")
+		return
+	}
 }
